7a646a6bf9b9a6b66714a9b324f223432b5a57cf
abelsolomon1 <142781217+abelsolomon1@users.noreply.github.com>
2024-02-11 23:23:45 +0100

    Add files via upload
    Push Uebung_Auto_Hausaufgabe von Abel Solomon

---
+++ Uebung_Auto/Uebung_Auto/Anlasser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_Auto
+{
+    internal class Anlasser
+    {
+        public void starten()
+        {
+            Console.WriteLine("Anlasser starten");
+        }
+
+        public void stoppen()
+        {
+            Console.WriteLine("Anlasser gestoppt");
+        }
+    }
+}
---
+++ Uebung_Auto/Uebung_Auto/Motor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_Auto
+{
+    internal class Motor 
+    {
+         Benzinpumpe benzinpumpe = new Benzinpumpe();
+
+         Anlasser anlasser= new Anlasser();
+
+        public void starten()
+        {
+            Console.WriteLine("Motor starten");
+
+            benzinpumpe.pumpen();
+            anlasser.starten();
+            regeln();
+
+
+            
+        }
+        private void regeln()
+        {
+            int i = 0;
+
+            while (i <= 6)
+            {
+                i++;
+
+                Console.WriteLine("Motor Regeln");
+
+                
+            }
+            anlasser.stoppen();
+
+            
+        }
+    }
+}
